52001571ebf4b360f1c5673c2951e749eff282df
Debasish Bhola <devop69@gmail.com>
2024-03-06 21:55:00 +0530

    linearSearch

---
+++ Arrays/linearSeach.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class linearSeach {
+    static int search(int[]arr,int target){
+        if (arr.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; //index
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] = {32,435,462,4234,13322,232,1,67};
+        System.out.println(search(arr, 462));
+    }
+}
