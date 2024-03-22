42a2ec34d3d8e315ba4ad841687f8d5b02c302b8
Debasish Bhola <devop69@gmail.com>
2024-03-22 10:21:00 +0530

    nextGreatestLetter

---
+++ Arrays/nextBigLetter.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class nextBigLetter {
+    static char nextGreatestLetter(char[] letters, char target){
+        int start = 0;
+        int end = letters.length-1;
+        while (start<=end) {
+            int mid = start + (end-start)/2;
+
+            if (target<letters[mid]) {
+                end = mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+    public static void main(String[] args) {
+        char[] arr = {'a','b','e','h'};
+        System.out.println(nextGreatestLetter(arr, 'c'));
+    }
+}
