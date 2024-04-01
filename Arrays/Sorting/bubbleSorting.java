4f03f677527563e503d8979d2da52bee84ec8f14
Debasish Bhola <devop69@gmail.com>
2024-04-02 00:19:24 +0530

    BubbleSort Done

---
+++ Arrays/Sorting/bubbleSorting.java
@@ -0,0 +1,24 @@
+package Arrays.Sorting;
+
+import java.util.Arrays;
+
+public class bubbleSorting {
+
+    public static void main(String[] args) {
+        int[] arr = {4,3,5,6,2,8,1};
+        BS(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void BS(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length-i; j++) {
+                if (arr[j]<arr[j-1]) {
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+    }
+}
