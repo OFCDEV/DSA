2529067676c69f5c156e5658f18ae0b2d338b45a
Debasish Bhola <devop69@gmail.com>
2024-05-05 10:28:04 +0530

    CyclicSort Completed

---
+++ Arrays/Sorting/cyclicSort.java
@@ -0,0 +1,31 @@
+//When given nos. from range 1,N=>Use Cyclic Sort
+//swap number with its correct index(index = value-1)
+package Arrays.Sorting;
+
+import java.util.Arrays;
+
+public class cyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,1,4};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int[] arr){
+        int i = 0;
+        while (i<arr.length) {
+            int correctIndex = arr[i]-1;
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr,i,correctIndex);
+            }else{
+                i++;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+}
