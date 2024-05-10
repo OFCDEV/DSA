d4a26dea9cc842ed08e6c6978fc0b8d6c7e378c5
Debasish Bhola <devop69@gmail.com>
2024-05-10 23:12:03 +0530

    FindAllSubsets

--- Strings/findAllSubsets.java
+++ Strings/findAllSubsets.java
@@ -1,5 +1,5 @@
 package Strings;
-
+import java.util.ArrayList;
 public class findAllSubsets {
     public static void main(String[] args) {
         subseq("", "abc");
@@ -13,5 +13,18 @@ public class findAllSubsets {
 
         subseq(p+ch, up.substring(1));
         subseq(p, up.substring(1));
+    }
+    static ArrayList<String> subseqArray(String p, String up){
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> left = subseqArray(p+ch, up.substring(1));
+        ArrayList<String> right = subseqArray(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
     }
 }
