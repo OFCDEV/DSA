af45e0b308237f095912cf89a182a4baa8f5effb
Debasish Bhola <devop69@gmail.com>
2024-06-28 20:38:32 +0530

    Find the number that appears once, and other numbers twice.

---
+++ Arrays/findTheNumThatAppearsOnce.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class findTheNumThatAppearsOnce {
+    static int bruteForce(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+            int count = 0;
+
+            for (int j = 0; j < arr.length; j++) {
+
+                if (arr[j] == num) {
+                    count++;
+                }
+
+                if (count == 1) return num;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]) {
+        int[] arr = {4, 1, 2, 1, 2};
+        System.out.println("The single element is: " + bruteForce(arr));
+
+    }
+}
