5df59449d80a83748a1e4742c73b1d6266aaae3c
Debasish Bhola <devop69@gmail.com>
2024-07-11 17:05:57 +0530

    Completed

--- Arrays/LongestSubarray.java
+++ Arrays/LongestSubarray.java
@@ -18,10 +18,31 @@ public class LongestSubarray {
         }
         return len;
     }
+    public static int getLongestSubarrayOptimal(int []a, long k) {
+        int n = a.length;
+        int left=0;
+        int right=0;
+        long sum = a[0];
+        int maxLen = 0;
+        while (right<n) {
+            //If sum>k,reduce the subarray from left
+            while (left<=right && sum>k) {
+                sum -=a[left];
+                left++;
+            }
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right-left+1);
+            }
+            right++; //Move the right pointer
+            if(right<n) sum+=a[right];
+
+        }
+        return maxLen;
+    }
     public static void main(String[] args) {
         int[] a = {2, 3, 5, 1, 9};
         long k = 10;
-        int len = getLongestSubarray(a, k);
+        int len = getLongestSubarrayOptimal(a, k);
         System.out.println("The length of the longest subarray is: " + len);
     }
 }
